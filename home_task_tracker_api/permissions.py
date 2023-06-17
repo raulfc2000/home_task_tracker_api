8339c5537a81782f3d6c3ba2936a78e3a84f61cf
Raúl F - PC <yanngelmarbella@gmail.com>
2023-06-17 13:39:00 +0200

    modificación README.md

---
+++ home_task_tracker_api/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+
+class IsSuperUser(permissions.BasePermission):
+    """
+    Permiso custom para identificar que es superusuario.
+    """
+    def has_permission(self, request, view):
+        user = request.user
+        if user and user.is_authenticated:
+            return user.is_superuser
+
+
+class UserInGroup(permissions.BasePermission):
+    """
+    Permiso custom para identificar que el user está en el Group que se está accediendo
+    """
+    def has_permission(self, request, view):
+        return False
+class IsMemberOfGroup(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user in obj.users_list.all()
