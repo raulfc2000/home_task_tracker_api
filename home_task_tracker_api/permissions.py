6aedcbc683819d5821e1e3cfd796b180273cf8e9
Raúl F - PC <yanngelmarbella@gmail.com>
2023-06-17 17:21:22 +0200

    permissions y urls

--- home_task_tracker_api/permissions.py
+++ home_task_tracker_api/permissions.py
@@ -6,21 +6,9 @@ class IsSuperUser(permissions.BasePermission):
     Permiso custom para identificar que es superusuario.
     """
     def has_permission(self, request, view):
+        # User
         user = request.user
+
+        # Si existe el usuario y está autenticado, es superusuario
         if user and user.is_authenticated:
             return user.is_superuser
-
-
-class UserInGroup(permissions.BasePermission):
-    """
-    Permiso custom para identificar que el user está en el Group que se está accediendo
-    """
-    def has_permission(self, request, view):
-        return False
-
-
-class IsMemberOfGroup(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        if user and user.is_authenticated:
-            return user in obj.users_list.all()
--- home_task_tracker_api/urls.py
+++ home_task_tracker_api/urls.py
@@ -44,4 +44,5 @@ urlpatterns = [
     dj_rest_auth_path_registration,
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('group/', include('apps.group.urls')),
 ]
